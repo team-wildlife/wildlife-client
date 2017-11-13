84b3f6b888489ffe15750f22e4c70343f337a609
QueensKid <andres.ibarra0716@gmail.com>
2017-11-13 14:32:27 -0800

    routes started

---
+++ scripts/models/wildlife.js
@@ -0,0 +1,56 @@
+'use strict';
+var app = app || {};
+
+((module) => {
+  var __API_URL__ = '';
+  animalView.all = [];
+
+  function errorCallback(err) {
+    console.error(err);
+    module.errorView.initErrorPage(err);
+  }
+
+  function Animal(rawDataObj) {
+    Object.keys(rawDataObj).forEach(key => this[key] = rawDataObj[key]);
+  }
+
+  Animal.prototype.toHtml = function() {
+    var template = Handlebars.compile($('#').text());
+    return template(this);
+  };
+
+  Animal.addDescription = function(data) {
+    var template = Handlebars.compile($('#').text());
+    return template(data);
+  };
+
+  Animal.loadAll = rawData => {
+    Animal.all = rawData.map(resultsObj => new Animal(resultsObj));
+  };
+
+  Animal.fetchAll = callback => {
+    console.log('fetchAll function called');
+    $.get(`${__API_URL__}/`)
+      .then(Animal.loadAll)
+      .then(callback)
+      .catch(errorCallback)
+  }
+
+  Animal.insertFormValues = data => {
+    $('#').attr('', data.id);
+    $('#').val(data.);
+    $('#').val(data.);
+    $('#').val(data.);
+    $('#').val(data.);
+    $('#').val(data.);
+  }
+
+  Animal.prototype.insertRecord = function(callback) {
+    $.post(`${__API_URL__}/`, {})
+      .then(app.Animal.fetchAll())
+      .then(callback)
+  };
+
+
+  module.Animal = Animal;
+})(app);
