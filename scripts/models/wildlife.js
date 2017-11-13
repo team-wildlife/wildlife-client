cac0f44f60170c1f978b07d06affc8296b4b24ef
Catherine Looper <loopercat@gmail.com>
2017-11-13 15:49:59 -0800

    Setting up ajax call for API

--- scripts/models/wildlife.js
+++ scripts/models/wildlife.js
@@ -5,7 +5,7 @@ var app = app || {};
 const __API_URL__ = 'http://localhost:3000';
 
 ((module) => {
-  animalView.all = [];
+  animalView.all = {};
 
   function errorCallback(err) {
     console.error(err);
@@ -52,15 +52,6 @@ const __API_URL__ = 'http://localhost:3000';
       .then(app.Animal.fetchAll())
       .then(callback)
   };
-
-  $.get('http://apiv3.iucnredlist.org/api/v3/country/getspecies/AZ?token=e66cfb0dfa5d5dab8984278503cec6af0bb18dc640e03fff2b7b796b69a97346')
-
-  .then(data => console.log(data))
-
-
-  $.get('http://apiv3.iucnredlist.org/api/v3/species/common_names/loxodonta%20africana?token=e66cfb0dfa5d5dab8984278503cec6af0bb18dc640e03fff2b7b796b69a97346')
-
-  .then(data => console.log(data))
 
 
   module.Animal = Animal;
--- scripts/views/wildlife-view.js
+++ scripts/views/wildlife-view.js
@@ -1,1 +1,28 @@
 'use strict'
+
+//
+// $.get('http://apiv3.iucnredlist.org/api/v3/country/getspecies/AZ?token=e66cfb0dfa5d5dab8984278503cec6af0bb18dc640e03fff2b7b796b69a97346')
+//
+// data => JSON.parse(data).forEach(repo =>
+//   $('#results').append(`<h3>${repo.name}</h3><p>${repo.description}</p><hr>`)),
+// data => console.log(JSON.parse(data)),
+// err => console.error(err.status, err.statusText, 'is the way my stuff is broken'));
+
+// .then(data => console.log(data))
+
+// const TOKEN = process.env.REDLIST_TOKEN;
+
+
+$.get(`http://apiv3.iucnredlist.org/api/v3/species/common_names/loxodonta%20africana?token=e66cfb0dfa5d5dab8984278503cec6af0bb18dc640e03fff2b7b796b69a97346`)
+
+// .then(data => console.log(data))
+  .then(
+    data => JSON.parse(data).forEach(animal => {
+      console.log(data)
+      $('#results').append(`<h3>${animal.taxonname}</h3><p>${animal.language}</p><hr>`)
+      // data => console.log(JSON.parse(data)),
+      err => console.error(err.status, err.statusText, 'is the way my stuff is broken')
+
+    })
+
+  )
