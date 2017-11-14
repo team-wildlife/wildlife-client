99e28c3d9a682b04014c11eafe3b1440add0902b
Catherine Looper <loopercat@gmail.com>
2017-11-13 19:51:26 -0800

    Modified wildlife-view.js

--- scripts/views/wildlife-view.js
+++ scripts/views/wildlife-view.js
@@ -1,14 +1,10 @@
 'use strict'
 
-
-$.get('http://apiv3.iucnredlist.org/api/v3/country/getspecies/AZ?token=e66cfb0dfa5d5dab8984278503cec6af0bb18dc640e03fff2b7b796b69a97346')
+$.get(`http://apiv3.iucnredlist.org/api/v3/country/getspecies/AZ?token=${REDLIST_TOKEN}`)
   .then(data => $('#results').append(`<h6>Scientific Name: ${data.result[0].scientific_name}</h6><p>Animal ID: ${data.result[0].taxonid}</p>`))
-// .then(data => console.log(data))
 
 
-
-const animalName = `http://apiv3.iucnredlist.org/api/v3/species/common_names/loxodonta%20africana?token=e66cfb0dfa5d5dab8984278503cec6af0bb18dc640e03fff2b7b796b69a97346`
-$.get(animalName)
+$.get(`http://apiv3.iucnredlist.org/api/v3/species/common_names/loxodonta%20africana?token=${REDLIST_TOKEN}`)
   .then(data => $('#results').append(`<h3>Animal: ${data.result[0].taxonname}</h3>`))
 
 // .then(data => console.log(data))
