d27b2aa2e58341fdd98b26d5f11fe3beeb9024a3
Catherine Looper <loopercat@gmail.com>
2017-11-14 09:45:56 -0800

    Drop down menu selection calls to the country name api call

--- scripts/views/wildlife-view.js
+++ scripts/views/wildlife-view.js
@@ -1,11 +1,21 @@
 'use strict'
 
-$.get(`http://apiv3.iucnredlist.org/api/v3/country/getspecies/AZ?token=${REDLIST_TOKEN}`)
-  .then(data => $('#results').append(`<h6>Scientific Name: ${data.result[0].scientific_name}</h6><p>Animal ID: ${data.result[0].taxonid}</p>`))
+const __API_URL__ = 'http://localhost:3000';
+
+$('select[name="country"]').on('change', function(event) {
+  var selectedCountry = event.target.value;
+  console.log(selectedCountry);
+  $.get(`${__API_URL__}/api/v1/countries/${selectedCountry}`)
+    .then(data => {
+      data = JSON.parse(data);
+      $('#results').append(`<h6>Scientific Name: ${data.result[0].scientific_name}</h6><p>Animal ID: ${data.result[0].taxonid}</p>`)
+    })
+})
 
 
-$.get(`http://apiv3.iucnredlist.org/api/v3/species/common_names/loxodonta%20africana?token=${REDLIST_TOKEN}`)
-  .then(data => $('#results').append(`<h3>Animal: ${data.result[0].taxonname}</h3>`))
+
+// $.get(`http://apiv3.iucnredlist.org/api/v3/species/common_names/loxodonta%20africana?token=${REDLIST_TOKEN}`)
+//   .then(data => $('#results').append(`<h3>Animal: ${data.result[0].taxonname}</h3>`))
 
 // .then(data => console.log(data))
   // .then(
@@ -18,3 +28,7 @@ $.get(`http://apiv3.iucnredlist.org/api/v3/species/common_names/loxodonta%20afri
   //   })
   //
   // )
+
+
+
+// Grabs the selected country ISO when country is selected and stores it in selectedCountry
