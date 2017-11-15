a318649b2a43e9bca6bae55c32a3048f7fd203fa
Catherine Looper <loopercat@gmail.com>
2017-11-14 17:29:38 -0800

    minor update

--- scripts/views/wildlife-view.js
+++ scripts/views/wildlife-view.js
@@ -1,7 +1,7 @@
 'use strict'
 
-// const __API_URL__ = 'http://localhost:3000';
-const __API_URL__ = 'https://team-wildlife.herokuapp.com'
+const __API_URL__ = 'http://localhost:3000';
+// const __API_URL__ = 'https://team-wildlife.herokuapp.com'
 let randomIndex;
 
 $('select[name="country"]').on('change', function(event) {
@@ -10,6 +10,7 @@ $('select[name="country"]').on('change', function(event) {
   $.get(`${__API_URL__}/api/v1/countries/${selectedCountry}`)
     .then(data => {
       data = JSON.parse(data);
+      console.log(data);
       randomIndex = Math.floor(Math.random()*data.result.length);
       $('#results-common').append(`<h6>Scientific Name: ${data.result[randomIndex].scientific_name}</h6><p>Animal ID: ${data.result[randomIndex].taxonid}</p>`)
       let commonName = data.result[randomIndex].scientific_name.toLowerCase().replace(' ', '%20');
