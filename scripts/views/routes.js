b43c9dc37a735eaaf90987cd3b3c47b664cefeef
Nick Gibson <Nicholas.a.Gibson@Gmail.com>
2017-11-16 21:48:22 -0800

    lousy git merges before

--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -1,11 +1,7 @@
 'use strict';
 var app = app || {};
 
-<<<<<<< HEAD
 page('/', () => app.animalView.initIndexPage);
-=======
-// page('/', () => app.animalView.initIndexPage);
->>>>>>> 8c688b61fe3f189ff31af8d2fe22e0aaf1c8261d
 page('/about', () => app.animalView.initAboutPage);
 page('/selectedCountry', () => app.animalView.selectedAnimalView);
 page('/speciesByCategory', () => app.animalView.speciesByCategoryView);
